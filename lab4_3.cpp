014f47c0a8dc4133bdf1fc9371cc6c6dd2a65150
lab302-38 <lab302-38@labeng.local>
2020-01-03 16:23:53 +0700

    v1

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
 
+int findDivisor (int x){
+    int i = 2;
+    while(i > 1){
+    	if(x%i == 0){
+    		return i;
+		}else{
+			i++;
+		}
+    	
+	}
+}
+
+int main(){
+	int x;
+	cout << "Enter Input = ";
+	cin >> x;
+	cout << "Expected Output = " << findDivisor(x);
+    
+    return 0;
+}
