6e8274f8026e5fafb022135c648cba7f14d1d9ab
lab302-38 <lab302-38@labeng.local>
2020-01-03 15:32:57 +0700

    v1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
 
+float Result (float x, float y, float e = 2.71828){
+    y = 3 * pow(x,3) + 2 * e + pow(2,2*x+1) + sqrt(pow(x,2)+1);
+    return y;
+}
+
+int main(){
+	float y, x;
+    cout << "Enter x = ";
+    cin >> x;
+    cout << "Result y = " << Result(x,y);
+    
+    return 0;
+}
